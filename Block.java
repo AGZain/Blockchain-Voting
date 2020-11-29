47736503c3d345e2628cbb466b4f813d90a9efbd
ooThisLooksFun <hgastronomer@gmail.com>
2020-11-29 16:11:24 -0500

    Implemented quadratic residue and changed usage in BlockChain

--- Block.java
+++ Block.java
@@ -3,7 +3,7 @@ public class Block {
     String timestamp;
     String id;
     String data;
-    PoW pow;
+    PoW pow;//Need Hash value, 8 length string maybe
     public Block(String timestamp, String id, String data, String prevHash) {
         this.timestamp = timestamp;
         this.id = id;
