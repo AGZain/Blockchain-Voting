977c58edc5b0e0493b7c000d1685bee857911455
CodeExplorer1999 <harshwardhan.goswami@ontariotechu.net>
2020-12-06 11:52:06 -0500

    Added Consensus Algorithm and fixed PoW

--- PoW.java
+++ PoW.java
@@ -19,8 +19,7 @@ public class PoW{
      * These are then stored in the res and non_res lists respectively
      */
     void buildProblems() {
-        //TODO: randomize n
-        int n = 500000;     //n = 500000 took my laptop around 14 seconds to solve    
+        int n = (int)(Math.random() * 550000) + 500000;     //n = 500000 took my laptop around 14 seconds to solve    
         ArrayList<Integer> x_list = new ArrayList<Integer>();   //list of all numbers co-prime of n
 
         /* Finding co primes for given n */
@@ -101,4 +100,34 @@ public class PoW{
                 }
         }   
     }
+    /* --------------------------------------------------------------------------------------
+     *                              Code for Consensus
+     *  --------------------------------------------------------------------------------------/
+     * Also lemme know when you start working on the implementation so I can help you with the
+     * difficulty part */
+    /**
+     * Check list if as are all valid by getting x from x = Math.sqrt(a % 10)
+     * And then check if x is co prime with n
+     * @param n given mod value
+     * @param a_list list of found residues
+     * @return Boolean if the answer is correct or incorrect
+     */ 
+    static Boolean checkAns(int n, ArrayList<Integer>a_list) {
+        for(int i = 0; i < a_list.size(); i++) {
+            double gcd = findGCD(a_list.get(i), n);
+            if(gcd != 1){ 
+                System.out.println(a_list.get(i) + ", " + n + ", " + gcd);
+                return false;
+            }
+        }
+        return true;
+    }
+    private static int findGCD(int num1, int num2) {
+        if(num2 == 0) {
+            return num1;
+        }
+        return findGCD(num2, num1 % num2);
+    }
+
+    
 }
